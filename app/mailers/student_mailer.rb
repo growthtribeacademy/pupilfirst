f509e0353762f9c251356dddb8637662002fb85d
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2021-05-31 22:39:26 +0200

    display level dates

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -3,8 +3,8 @@ class StudentMailer < SchoolMailer
     @school = student.course.school
     @course = student.course
     @student = student
-    @levels = @course.levels
-    @course_start_date = @levels.where.not(unlock_at: nil).pluck(:unlock_at).min
+    @levels = @course.levels.where.not(unlock_at: nil)
+    @course_start_date = @levels.pluck(:unlock_at).min
     @course_end_date = @course.ends_at
 
     simple_roadie_mail(@student.email, "You have been added as a student in #{@school.name}")
