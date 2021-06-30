f772e58209a7e8bed4fd45c836f7a28f2eaa562f
Mirosław Pragłowski <m@praglowski.com>
2021-06-30 11:41:36 +0200

    Script to automatically level up students

---
+++ scripts/auto_level_up.rb
@@ -0,0 +1,36 @@
+
+class AutoLevelUp
+  def call(course_id)
+    course = Course.find(course_id)
+
+    course.startups.each do |team|
+      ApplicationRecord.transaction do
+        next unless completed?(team.level, team.founders)
+        level_up(course, team)
+      end
+    end
+  end
+
+  private
+
+  def completed?(level, founders)
+    level.targets.all?{|t| target_completed?(t, founders)}
+  end
+
+  def target_completed?(target, founders)
+    founders.all?{|f| target_completed_by?(target, f)}
+  end
+
+  def target_completed_by?(target, founder)
+    target.status(founder).in?([Targets::StatusService::STATUS_PASSED])
+  end
+
+  def level_up(course, team)
+    puts "Level up from #{team.level.number} - #{team.name}"
+    next_level = course.levels.find_by(number: team.level.number + 1)
+    raise unless next_level
+    team.update!(level: next_level)
+  end
+end
+
+AutoLevelUp.new.call(ARGV[0])
