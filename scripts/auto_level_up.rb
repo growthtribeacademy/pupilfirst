38ad58b9d1087ed65c884f6f98ffe24edf971e13
Mirosław Pragłowski <m@praglowski.com>
2021-07-15 12:24:06 +0200

    Let's code this only once

--- scripts/auto_level_up.rb
+++ scripts/auto_level_up.rb
@@ -6,44 +6,9 @@ class AutoLevelUp
 
     course.startups.each do |team|
       ApplicationRecord.transaction do
-        next unless completed?(team.level, team.founders)
-        level_up(course, team)
+        LevelProgressionCheck::Internal.new(course, team.level, team)
       end
     end
-  end
-
-  private
-
-  def completed?(level, founders)
-    level.targets.all?{|t| target_completed?(t, founders)}
-  end
-
-  def target_completed?(target, founders)
-    founders.all?{|f| target_completed_by?(target, f)}
-  end
-
-  def target_completed_by?(target, founder)
-    target.status(founder).in?(eligible_statuses)
-  end
-
-  def level_up(course, team)
-    up = next_level(course, team.level)
-    return unless up
-
-    puts "Level up from #{team.level.number} - #{team.name}"
-    team.update!(level: up)
-  end
-
-  def next_level(course, current_level)
-    course.levels.find_by(number: current_level.number + 1)
-  end
-
-  def eligible_statuses
-    [
-      Targets::StatusService::STATUS_SUBMITTED,
-      Targets::StatusService::STATUS_PASSED,
-      Targets::StatusService::STATUS_FAILED
-    ]
   end
 end
 
